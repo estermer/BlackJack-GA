7ec09fe60faa553fa390c9b1532ad93a6aa53938
estermer <eric.stermer@gmail.com>
2016-09-16 13:14:25 -0400

    Added more functionality  to GAME OVER scenarios and card flip animation

--- js/blackjack.js
+++ js/blackjack.js
@@ -20,7 +20,6 @@ var gameStats = {
   playerScore: 0,
   dealerHand:[],
   dealerScore: 0,
-  isGameOver: false,
   playersTurn: true,
   playerBust: false,
   cardsAreDealt: false,
@@ -28,58 +27,46 @@ var gameStats = {
   dealerWins: false
 };
 
-/*
-Create an App object that deals with the logic of the game
-  gameIsOver - function that checks the bank account if anything is lefft in it and changes isGameOver to true
-  newGame - function that clears and reinitializes the decks, isGameOver and playerrs bank
-  shuffleCards - checks if deck is empty, moves all cards from discard pile to dealerDeck and shuffles their order
-  dealCard- checks whose turn it is, takes a card from deck and pushes it to player or computers hand
-  newHand - when deal button is hit deal cards in alternating order by looping 4 times, calling dealCard,
-    changin player turn after each card dealt
-    on second card dealt only, diplay card facedown
-  discardHand - after hand is played empties comp and player hands, puts cards in discardDeck
-  playerBet - adds bet to betAmmount and removes it from playerBank
-  adjustPlayerBank - when player sets bet remove money from bank, add winnings back to bank
-  flipCard - change status of card to either showing or not
-*/
-
 var App = {
 
-  cards: [{cardName: 'ace', cardValue: 11, img: 'img/acespades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'two', cardValue: 2, img: 'img/twospades.jpeg', suit: 'spades', faceUp: false},
-    {cardName: 'three', cardValue: 3, img: 'img/threespades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'four', cardValue: 4, img: 'img/fourspades.jpeg', suit: 'spades', faceUp: false},
-    {cardName: 'five', cardValue: 5, img: 'img/fivespades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'six', cardValue: 6, img: 'img/sixspades.jpeg', suit: 'spades', faceUp: false},
-    {cardName: 'seven', cardValue: 7, img: 'img/sevenspades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'eight', cardValue: 8, img: 'img/eightspades.jpeg', suit: 'spades', faceUp: false},
-    {cardName: 'nine', cardValue: 9, img: 'img/ninespades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'ten', cardValue: 10, img: 'img/tenspades.jpeg', suit:'spades' , faceUp: false},
-    {cardName: 'jack', cardValue: 10, img: 'img/jackspades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'queen', cardValue: 10, img: 'img/queenspades.jpeg', suit: 'spades', faceUp: false},
-    {cardName: 'king', cardValue: 10, img: 'img/kingspades.jpeg', suit: 'spades', faceUp: false}, {cardName: 'ace', cardValue: 11, img: 'img/aceclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'two', cardValue: 2, img: 'img/twoclubs.jpeg', suit: 'clubs', faceUp: false}, {cardName: 'three', cardValue: 3, img: 'img/threeclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'four', cardValue: 4, img: 'img/fourclubs.jpeg', suit: 'clubs', faceUp: false}, {cardName: 'five', cardValue: 5, img: 'img/fiveclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'six', cardValue: 6, img: 'img/sixclubs.jpeg', suit: 'clubs', faceUp: false}, {cardName: 'seven', cardValue: 7, img: 'img/sevenclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'eight', cardValue: 8, img: 'img/eightclubs.jpeg', suit: 'clubs', faceUp: false}, {cardName: 'nine', cardValue: 9, img: 'img/nineclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'ten', cardValue: 10, img: 'img/tenclubs.jpeg', suit: 'clubs', faceUp: false}, {cardName: 'jack', cardValue: 10, img: 'img/jackclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'queen', cardValue: 10, img: 'img/queenclubs.jpeg', suit: 'clubs', faceUp: false}, {cardName: 'king', cardValue: 10, img: 'img/kingclubs.jpeg', suit: 'clubs', faceUp: false},
-    {cardName: 'ace', cardValue: 11, img: 'img/acehearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'two', cardValue: 2, img: 'img/twohearts.jpeg', suit: 'hearts', faceUp: false},
-    {cardName: 'three', cardValue: 3, img: 'img/threehearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'four', cardValue: 4, img: 'img/fourhearts.jpeg', suit: 'hearts', faceUp: false},
-    {cardName: 'five', cardValue: 5, img: 'img/fivehearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'six', cardValue: 6, img: 'img/sixhearts.jpeg', suit: 'hearts', faceUp: false},
-    {cardName: 'seven', cardValue: 7, img: 'img/sevenhearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'eight', cardValue: 8, img: 'img/eighthearts.jpeg', suit: 'hearts', faceUp: false},
-    {cardName: 'nine', cardValue: 9, img: 'img/ninehearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'ten', cardValue: 10, img: 'img/tenhearts.jpeg', suit: 'hearts', faceUp: false},
-    {cardName: 'jack', cardValue: 10, img: 'img/jackhearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'queen', cardValue: 10, img: 'img/queenhearts.jpeg', suit: 'hearts', faceUp: false},
-    {cardName: 'king', cardValue: 10, img: 'img/kinghearts.jpeg', suit: 'hearts', faceUp: false}, {cardName: 'ace', cardValue: 11, img: 'img/acediamonds.jpeg', suit: 'diamonds', faceUp: false},
-    {cardName: 'two', cardValue: 2, img: 'img/twodiamonds.jpeg', suit: 'diamonds', faceUp: false}, {cardName: 'three', cardValue: 3, img: 'img/threediamonds.jpeg', suit: 'diamonds', faceUp: false},
-    {cardName: 'four', cardValue: 4, img: 'img/fourdiamonds.jpeg', suit: 'diamonds', faceUp: false}, {cardName: 'five', cardValue: 5, img: 'img/fivediamonds.jpeg', suit: 'diamonds', faceUp: false},
-    {cardName: 'six', cardValue: 6, img: 'img/sixdiamonds.jpeg', suit: 'diamonds', faceUp: false}, {cardName: 'seven', cardValue: 7, img: 'img/sevendiamonds.jpeg', suit: 'diamonds', faceUp: false},
-    {cardName: 'eight', cardValue: 8, img: 'img/eightdiamonds.jpeg', suit: 'diamonds', faceUp: false}, {cardName: 'nine', cardValue: 9, img: 'img/ninediamonds.jpeg', suit: 'diamonds', faceUp: false},
-    {cardName: 'ten', cardValue: 10, img: 'img/tendiamonds.jpeg', suit: 'diamonds', faceUp: false}, {cardName: 'jack', cardValue: 10, img: 'img/jackdiamonds.jpeg', suit: 'diamonds', faceUp: false},
-    {cardName: 'queen', cardValue: 10, img: 'img/queendiamonds.jpeg', suit: 'diamonds', faceUp: false}, {cardName: 'king', cardValue: 10, img: 'img/kingdiamonds.jpeg', suit: 'diamonds', faceUp: false}],
+  cards: [{cardName: 'ace', cardValue: 11, img: 'img/acespades.jpeg', suit: 'spades'}, {cardName: 'two', cardValue: 2, img: 'img/twospades.jpeg', suit: 'spades'},
+    {cardName: 'three', cardValue: 3, img: 'img/threespades.jpeg', suit: 'spades'}, {cardName: 'four', cardValue: 4, img: 'img/fourspades.jpeg', suit: 'spades'},
+    {cardName: 'five', cardValue: 5, img: 'img/fivespades.jpeg', suit: 'spades'}, {cardName: 'six', cardValue: 6, img: 'img/sixspades.jpeg', suit: 'spades'},
+    {cardName: 'seven', cardValue: 7, img: 'img/sevenspades.jpeg', suit: 'spades'}, {cardName: 'eight', cardValue: 8, img: 'img/eightspades.jpeg', suit: 'spades'},
+    {cardName: 'nine', cardValue: 9, img: 'img/ninespades.jpeg', suit: 'spades'}, {cardName: 'ten', cardValue: 10, img: 'img/tenspades.jpeg', suit:'spades'},
+    {cardName: 'jack', cardValue: 10, img: 'img/jackspades.jpeg', suit: 'spades'}, {cardName: 'queen', cardValue: 10, img: 'img/queenspades.jpeg', suit: 'spades'},
+    {cardName: 'king', cardValue: 10, img: 'img/kingspades.jpeg', suit: 'spades'}, {cardName: 'ace', cardValue: 11, img: 'img/aceclubs.jpeg', suit: 'clubs'},
+    {cardName: 'two', cardValue: 2, img: 'img/twoclubs.jpeg', suit: 'clubs'}, {cardName: 'three', cardValue: 3, img: 'img/threeclubs.jpeg', suit: 'clubs'},
+    {cardName: 'four', cardValue: 4, img: 'img/fourclubs.jpeg', suit: 'clubs'}, {cardName: 'five', cardValue: 5, img: 'img/fiveclubs.jpeg', suit: 'clubs'},
+    {cardName: 'six', cardValue: 6, img: 'img/sixclubs.jpeg', suit: 'clubs'}, {cardName: 'seven', cardValue: 7, img: 'img/sevenclubs.jpeg', suit: 'clubs'},
+    {cardName: 'eight', cardValue: 8, img: 'img/eightclubs.jpeg', suit: 'clubs'}, {cardName: 'nine', cardValue: 9, img: 'img/nineclubs.jpeg', suit: 'clubs'},
+    {cardName: 'ten', cardValue: 10, img: 'img/tenclubs.jpeg', suit: 'clubs'}, {cardName: 'jack', cardValue: 10, img: 'img/jackclubs.jpeg', suit: 'clubs'},
+    {cardName: 'queen', cardValue: 10, img: 'img/queenclubs.jpeg', suit: 'clubs'}, {cardName: 'king', cardValue: 10, img: 'img/kingclubs.jpeg', suit: 'clubs'},
+    {cardName: 'ace', cardValue: 11, img: 'img/acehearts.jpeg', suit: 'hearts'}, {cardName: 'two', cardValue: 2, img: 'img/twohearts.jpeg', suit: 'hearts'},
+    {cardName: 'three', cardValue: 3, img: 'img/threehearts.jpeg', suit: 'hearts'}, {cardName: 'four', cardValue: 4, img: 'img/fourhearts.jpeg', suit: 'hearts'},
+    {cardName: 'five', cardValue: 5, img: 'img/fivehearts.jpeg', suit: 'hearts'}, {cardName: 'six', cardValue: 6, img: 'img/sixhearts.jpeg', suit: 'hearts'},
+    {cardName: 'seven', cardValue: 7, img: 'img/sevenhearts.jpeg', suit: 'hearts'}, {cardName: 'eight', cardValue: 8, img: 'img/eighthearts.jpeg', suit: 'hearts'},
+    {cardName: 'nine', cardValue: 9, img: 'img/ninehearts.jpeg', suit: 'hearts'}, {cardName: 'ten', cardValue: 10, img: 'img/tenhearts.jpeg', suit: 'hearts'},
+    {cardName: 'jack', cardValue: 10, img: 'img/jackhearts.jpeg', suit: 'hearts'}, {cardName: 'queen', cardValue: 10, img: 'img/queenhearts.jpeg', suit: 'hearts'},
+    {cardName: 'king', cardValue: 10, img: 'img/kinghearts.jpeg', suit: 'hearts'}, {cardName: 'ace', cardValue: 11, img: 'img/acediamonds.jpeg', suit: 'diamonds'},
+    {cardName: 'two', cardValue: 2, img: 'img/twodiamonds.jpeg', suit: 'diamonds'}, {cardName: 'three', cardValue: 3, img: 'img/threediamonds.jpeg', suit: 'diamonds'},
+    {cardName: 'four', cardValue: 4, img: 'img/fourdiamonds.jpeg', suit: 'diamonds'}, {cardName: 'five', cardValue: 5, img: 'img/fivediamonds.jpeg', suit: 'diamonds'},
+    {cardName: 'six', cardValue: 6, img: 'img/sixdiamonds.jpeg', suit: 'diamonds'}, {cardName: 'seven', cardValue: 7, img: 'img/sevendiamonds.jpeg', suit: 'diamonds'},
+    {cardName: 'eight', cardValue: 8, img: 'img/eightdiamonds.jpeg', suit: 'diamonds'}, {cardName: 'nine', cardValue: 9, img: 'img/ninediamonds.jpeg', suit: 'diamonds'},
+    {cardName: 'ten', cardValue: 10, img: 'img/tendiamonds.jpeg', suit: 'diamonds'}, {cardName: 'jack', cardValue: 10, img: 'img/jackdiamonds.jpeg', suit: 'diamonds'},
+    {cardName: 'queen', cardValue: 10, img: 'img/queendiamonds.jpeg', suit: 'diamonds'}, {cardName: 'king', cardValue: 10, img: 'img/kingdiamonds.jpeg', suit: 'diamonds'}],
   newGame: function(){
-    gameStats.isGameOver = false;
+
+    // set all stats to starting points
     gameStats.playersTurn = true;
     gameStats.cardsAreDealt = false;
     gameStats.playerHand = [];
     gameStats.playerScore = 0;
     gameStats.dealerHand = [];
     gameStats.dealerScore = 0;
+    gameStats.currentBet = 0;
     gameStats.playerBankAmmount = 2000;
+
     //shuffleCards and input to the deck
     gameStats.cards = this.shuffleCards();
   },
@@ -129,10 +116,10 @@ var App = {
     //send card to player or dealer and create card on DOM
     if(gameStats.playersTurn){
       gameStats.playerHand.push(card);
-      UI.createPlayerCard(gameStats.playerHand);
+      UI.createPlayerCard(card);
     } else {
       gameStats.dealerHand.push(card);
-      UI.createDealerCard(gameStats.dealerHand);
+      UI.createDealerCard(card);
     }
   },
   discardHand: function(){
@@ -148,6 +135,12 @@ var App = {
     //subtract bet ammount from player bank
     gameStats.playerBankAmmount -= gameStats.betAmmount;
 
+    //if bank ammount goes below zero, dont allow the bet to be made
+    if(gameStats.playerBankAmmount < 0){
+      gameStats.playerBankAmmount += gameStats.betAmmount;
+      gameStats.currentBet -= gameStats.betAmmount;
+    }
+
   },
   doubleBet: function() {
     //add current bet back to player bank
@@ -158,16 +151,16 @@ var App = {
 
     //subtract current bet ammount from player bank
     gameStats.playerBankAmmount -= gameStats.currentBet;
+
+    //if bank ammount goes below zero, dont allow the bet to be made
+    if(gameStats.playerBankAmmount < 0){
+      gameStats.playerBankAmmount += gameStats.currentBet;
+      gameStats.currentBet /= 2;
+      gameStats.playerBankAmmount -= gameStats.currentBet;
+    }
   },
   playerWinnings: function(){
     gameStats.playerBankAmmount += gameStats.currentBet * 2;
-  },
-  flipCard: function(card){
-    if(card.faceUp){
-      card.faceUp = false;
-    } else {
-      card.faceUp = true;
-    }
   },
   dealerTurn: function(){
     //second check for blackJack
@@ -186,11 +179,11 @@ var App = {
         //check if dealer busted
         gameStats.cardsAreDealt = false;
         this.playerWinnings();
-        UI.playerBankDisplay();
+        // UI.playerBankDisplay();
         UI.winDisplay();
-        gameStats.currentBet = 0;
+        // gameStats.currentBet = 0;
         gameStats.playersTurn = true;
-        UI.playerBankDisplay();
+        // UI.playerBankDisplay();
 
       } else if(gameStats.dealerScore > 17 && gameStats.dealerScore < gameStats.playerScore) {
 
@@ -199,9 +192,8 @@ var App = {
         this.playerWinnings();
         UI.playerBankDisplay();
         UI.winDisplay();
-        gameStats.currentBet = 0;
         gameStats.playersTurn = true;
-        UI.playerBankDisplay();
+        // UI.playerBankDisplay();
 
       } else if (gameStats.dealerScore === gameStats.playerScore && gameStats.dealerScore > 11){
 
@@ -209,26 +201,32 @@ var App = {
         gameStats.cardsAreDealt = false;
         UI.tieDisplay();
         gameStats.playerBankAmmount += gameStats.currentBet;
-        gameStats.currentBet = 0;
         gameStats.playersTurn = true;
-        UI.playerBankDisplay();
+        // UI.playerBankDisplay();
 
       } else if(gameStats.dealerScore > gameStats.playerScore){
 
         //check if dealer score is higher than players
         gameStats.cardsAreDealt = false;
         UI.loseDisplay();
-        gameStats.currentBet = 0;
         gameStats.playersTurn = true;
 
       } else {
 
-        //otherwise hit
-        this.dealCard();
+        // deal card
+        App.dealCard();
+        App.computeHand(gameStats.dealerHand);
 
       }
 
     }
+
+    UI.playerBankDisplay();
+    gameStats.currentBet = 0;
+
+    //check if player is out of money and announce Game Over
+    App.outOfMoneyGameOver();
+
   },
   computeHand: function(hand) {
     var sum = 0;
@@ -265,70 +263,62 @@ var App = {
     }
     return blackJack;
   },
-  // checkForWinner: function() {
-  //   if()
-  // }
+  outOfMoneyGameOver: function() {
+    if(gameStats.playerBankAmmount === 0){
+      gameStats.playersTurn = false;
+      gameStats.cardsAreDealt = true;
+      alert('GAME OVER! PRESS START FOR A NEW GAME!');
+    }
+  }
 };
-
-
-/*
-Create an UI object to deal with the HTML of the game
-  createCard a function that will take in an argument of the card object and return the cardHTML
-  slideCard is a function that will translate the card from the deck to the player or dealer spot on table
-  clearTable will clear the cards from the table
-  postBet will post the bet to the dissplay
-  playerBankDisplay will display the ammount in the players bank
-  winOrLoseDisplay
-  gameOverDisplay
-*/
 
 var UI = {
 
-  createPlayerCard: function(hand){
-    //reset html
-    $('#player-hand').html('');
+  createPlayerCard: function(card){
 
-    //loop through hand and set html and append
-    for(var i = 0; i < hand.length; i++){
-      var $card = $('<div>').addClass('card');
-      var img = hand[i].img;
-      $card.html('<img class="card" src="' + img + '">');
+    var $card = $('<div>').addClass('card');
+    var img = card.img;
+    $card.html('<img class="card" src="' + img + '">');
 
-      //append to the DOM
-      $('#player-hand').append($card);
-    }
-    App.computeHand(hand);
+    //append to the DOM
+    $('#player-hand').append($card);
+
   },
-  createDealerCard: function(hand) {
-    //reset html
-    $('#dealer-hand').html('');
+  createDealerCard: function(card) {
 
-    //loop through hand and set html and append
-    for(var i = 0; i < hand.length; i++){
       var img;
+      var children = document.getElementById('dealer-hand').children;
 
       //make the first card face down
-      if(i === 0 && hand[i].faceUp === false){
+      if(children.length === 0 ){
         img = 'img/cardback.jpg';
       } else {
-        img = hand[i].img;
+        img = card.img;
       }
 
+      //add the class and img html
       var $card = $('<div>').addClass('card');
       $card.html('<img class="card" src="' + img + '">');
 
       //append to the DOM
       $('#dealer-hand').append($card);
-    }
-    //computer dealers hand
-    App.computeHand(hand);
+
   },
-  slideCard: function(){
+  showDealerCard: function() {
+    var children = document.getElementById('dealer-hand').children;
+    var img = gameStats.dealerHand[0].img;
+
+    //rotate card by adding a class
+    children[0].innerHTML = '<img class="card begin-flip-card" src="img/cardback.jpg">';
+
+    var delay = setTimeout(function() {
+      //fill ith the card image
+      children[0].innerHTML = '<img class="card end-flip-card" src="' + img + '">';
+    }, 400);
 
   },
   clearTable: function(){
     //set inner html of both hands to nothing
-    // gameStats.dealerHand[0].faceUp = true;
     $('#dealer-hand').html('');
     $('#player-hand').html('');
     $('#game-feedback').html('<span>PLACE YOUR BET, THEN PRESS DEAL</span>');
@@ -371,16 +361,6 @@ var UI = {
 
 };
 
-/*
-Create an events object to be event handlers
-  Start game - initializes players money
-  Deal - deals cards to dealer and player
-  Bet - allows a user to bet money just before the hand
-  DoubleDown - allows user to double down on a handlers
-  Hit - asks dealer to deal player another cards
-
-*/
-
 var Events = {
 
   startGame: function() {
@@ -396,11 +376,19 @@ var Events = {
     if(!gameStats.cardsAreDealt){
       //clearTable and hands
       App.discardHand();
+
+      var animationClass =  'deal-card-player';
+
       for(var i = 0; i < 4; i++){
+
         App.dealCard();
+
+
         if(gameStats.playersTurn){
+          animationClass =  'deal-card-dealer';
           gameStats.playersTurn = false;
         } else {
+          animationClass =  'deal-card-player';
           gameStats.playersTurn = true;
         }
       }
@@ -414,6 +402,7 @@ var Events = {
         gameStats.currentBet = 0;
       }
 
+      App.computeHand(gameStats.playerHand);
       UI.displayGameStatus(gameStats.playerScore);
 
       //prevent user from pressing deal until cards need to be dealt again
@@ -445,7 +434,12 @@ var Events = {
       //player only allowed to hit up to 5 cards
       if(gameStats.playerHand.length < 5){
 
+        // deal card
         App.dealCard();
+        //compute the hand
+        App.computeHand(gameStats.playerHand);
+
+
         UI.displayGameStatus(gameStats.playerScore);
 
         //check if hand busted
@@ -456,6 +450,9 @@ var Events = {
           gameStats.cardsAreDealt = false;
           gameStats.playersTurn = true;
           gameStats.currentBet = 0;
+
+          //check if player is out of money and announce Game Over
+          App.outOfMoneyGameOver();
         }
 
       } else {
@@ -474,10 +471,10 @@ var Events = {
       gameStats.playersTurn = false;
 
       //flipping facedown card over for dealer
-      gameStats.dealerHand[0].faceUp = true;
-      UI.createDealerCard(gameStats.dealerHand)
+      UI.showDealerCard();
 
       //call dealers turn
+      App.computeHand(gameStats.dealerHand);
       App.dealerTurn();
     }
   }
